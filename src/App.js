90e579b82de2a09731e8877ab38458929e7be0b3
Issam eddine Bouhouch <80135610+issam-eddine-bouhouch@users.noreply.github.com>
2021-03-12 02:27:54 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.scss';
+import Todo from './components/Todo'
+
+function App() {
+  return (
+    <div className="App container">
+      <Todo />
+    </div>
+  );
+}
+
+
+
+
+
+
+export default App;
